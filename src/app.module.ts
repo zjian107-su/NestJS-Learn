aab82f47002182cd09181124affb585169290c86
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2024-03-20 15:47:22 +0000

    :sparkles: feat: prisma migrate dev, prisma module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, PrismaModule],
   controllers: [AppController],
 })
 export class AppModule {}
