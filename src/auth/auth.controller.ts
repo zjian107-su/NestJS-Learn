aba27c15851e285140a59a68519aa5e48f3a907c
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2024-03-24 16:32:50 +0000

    featuers: controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Req() req: Request) {
+    console.log(req.body);
+    return this.authService.signup();
+  }
 
   @Post('signin')
   signin() {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,10 @@ export class AuthService {
     console.log('Daniel');
   }
 
+  signup() {
+    return { message: 'user signed up' };
+  }
+
   signin() {
     return { message: 'user signed in' };
   }
