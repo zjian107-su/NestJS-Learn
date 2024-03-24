74be2713523475a03cb66a0c2c07204a9295ee6e
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2024-03-24 21:37:13 +0000

    :sparkles: feat: validator decorator

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,13 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  // validator decorators for email
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  // validator decorators for password
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
